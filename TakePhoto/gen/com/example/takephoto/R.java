a05834befb2d861e50183cd71de902462cfa4021
hwealnegn <hwealnegn@gmail.com>
2014-11-06 22:11:15 +0800

    Updated TakePhoto

--- TakePhoto/gen/com/example/takephoto/R.java
+++ TakePhoto/gen/com/example/takephoto/R.java
@@ -25,12 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_gallery=0x7f080004;
-        public static final int action_settings=0x7f080002;
-        public static final int action_take_photo=0x7f080003;
+        public static final int action_gallery=0x7f080007;
+        public static final int action_settings=0x7f080005;
+        public static final int action_take_photo=0x7f080006;
         public static final int imageView1=0x7f080001;
+        public static final int linearLayout1=0x7f080004;
+        public static final int scrollView1=0x7f080003;
         public static final int textView1=0x7f080000;
-		public static int linearLayout1;
+        public static final int view1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
