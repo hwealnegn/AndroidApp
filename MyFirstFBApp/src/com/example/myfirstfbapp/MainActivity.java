d7f424dff11e6cebc211a385883d1de2588662b8
hwealnegn <hwealnegn@gmail.com>
2014-11-17 22:12:10 +0800

    Updated MyFirstFBApp

--- MyFirstFBApp/src/com/example/myfirstfbapp/MainActivity.java
+++ MyFirstFBApp/src/com/example/myfirstfbapp/MainActivity.java
@@ -1,25 +1,65 @@
 package com.example.myfirstfbapp;
 
 import com.facebook.AppEventsLogger;
+import com.facebook.Request;
+import com.facebook.Request.GraphUserCallback;
+import com.facebook.Response;
 import com.facebook.Session;
+import com.facebook.Session.StatusCallback;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView textView;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        textView = (TextView) findViewById(R.id.textView);
         
+        Session.openActiveSession(this,  true,  new StatusCallback() {
+
+			@Override
+			public void call(Session session, SessionState state,
+					Exception exception) {
+				// TODO Auto-generated method stub
+				if (session.isOpened()) {
+					Request.newMeRequest(session,  new GraphUserCallback() {
+
+						@Override
+						public void onCompleted(GraphUser user,
+								Response response) {
+							// TODO Auto-generated method stub
+							if (user != null) {
+								String name = user.getName();
+								textView.setText(name);
+							}
+						}
+						
+					});
+				}
+			}
+        	
+        });
     }
 
-
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode,
+				resultCode, data);
+	}
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
